de13709b63d82cbe109cda81c027cf7f8323fdb9
NiPeGun <nipegun@gmail.com>
2025-02-11 18:33:16 +0100

    Update PlantaQuimica-Crear.sh

--- CETI/SegInd/PlantaQuimica-Crear.sh
+++ CETI/SegInd/PlantaQuimica-Crear.sh
@@ -5,6 +5,7 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
+
 # ----------
 # Script de NiPeGun para descargar e importar el pack PlantaQuimica para VirtualBox en Debian
 #
